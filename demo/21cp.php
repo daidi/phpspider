a737fe91529e02c741328c0315f855289c896ef6
PapacyDai <daidi88@sina.com>
2017-01-22 20:21:08 +0800

    c

--- demo/21cp.php
+++ demo/21cp.php
@@ -1,6 +1,6 @@
 <?php
 ini_set("memory_limit", "1024M");
-require dirname(__FILE__).'/../core/init.php';
+require dirname(__FILE__) . '/../core/init.php';
 
 /* Do NOT delete this comment */
 /* 不要删除这段注释 */
@@ -17,62 +17,59 @@ $configs = array(
     'scan_urls' => array(
         'http://info.21cp.com/'
     ),
-    'list_url_regexes' => array(
-    ),
+    'list_url_regexes' => array(),
     'content_url_regexes' => array(
         "http://info.21cp.com/industry.*/201\d+/\d+.htm",
     ),
     'max_try' => 5,
+//    'export' => array(
+//        'type' => 'csv',
+//        'file' => PATH_DATA.'/qiushibaike.csv',
+//    ),
     //'export' => array(
-        //'type' => 'csv',
-        //'file' => PATH_DATA.'/qiushibaike.csv',
-    //),
-    //'export' => array(
-        //'type'  => 'sql',
-        //'file'  => PATH_DATA.'/qiushibaike.sql',
-        //'table' => 'content',
+    //'type'  => 'sql',
+    //'file'  => PATH_DATA.'/qiushibaike.sql',
+    //'table' => 'content',
     //),
     'export' => array(
-        'type' => 'db', 
-        'table' => 'content',
+        'type' => 'db',
+        'table' => '21cn',
     ),
     'fields' => array(
         array(
             'name' => "article_title",
             'selector' => "/html/body/div[6]/div[2]/div[1]/h2",
-            'required' => true,
+            'required' => false,
         ),
         array(
             'name' => "article_author",
             'selector' => "/html/body/div[6]/div[2]/div[1]/ul/li[2]",
-            'required' => true,
+            'required' => false,
         ),
         array(
             'name' => "article_content",
             'selector' => "/html/body/div[6]/div[2]/div[3]",
-            'required' => true,
+            'required' => false,
         ),
         array(
             'name' => "article_publish_time",
             'selector' => "/html/body/div[6]/div[2]/div[1]/ul/li[1]",
-            'required' => true,
+            'required' => false,
         ),
         array(
             'name' => "url",
             'selector' => "",   // 这里随便设置，on_extract_field回调里面会替换
-            'required' => true,
+            'required' => false,
         ),
     ),
 );
 
 $spider = new phpspider($configs);
 
-$spider->on_handle_img = function($fieldname, $img) 
-{
+$spider->on_handle_img = function ($fieldname, $img) {
     $regex = '/src="(https?:\/\/.*?)"/i';
     preg_match($regex, $img, $rs);
-    if (!$rs) 
-    {
+    if (!$rs) {
         return $img;
     }
 
@@ -83,7 +80,7 @@ $spider->on_handle_img = function($fieldname, $img)
     //$fileext = $pathinfo['extension'];
     //if (strtolower($fileext) == 'jpeg') 
     //{
-        //$fileext = 'jpg';
+    //$fileext = 'jpg';
     //}
     //// 以纳秒为单位生成随机数
     //$filename = uniqid().".".$fileext;
@@ -97,34 +94,26 @@ $spider->on_handle_img = function($fieldname, $img)
     return $img;
 };
 
-$spider->on_extract_field = function($fieldname, $data, $page) 
-{
+$spider->on_extract_field = function ($fieldname, $data, $page) {
     $encoding = util::get_encoding($page['raw']);
-    if ($encoding == 'iso-8859-1') 
-    {
+    if ($encoding == 'iso-8859-1') {
         //$data = mb_convert_encoding($data, "LATIN1", "UTF-8");
         //用 UTF-8 编码的数据解码为 ISO-8859-1 编码
         $data = utf8_decode($data);
     }
 
-    if ($fieldname == 'article_title') 
-    {
-        if (strlen($data) > 10) 
-        {
+    if ($fieldname == 'article_title') {
+        if (strlen($data) > 10) {
             // 下面方法截取中文会有异常
             //$data = substr($data, 0, 10)."...";
-            $data = mb_substr($data, 0, 10, 'UTF-8')."...";
+            $data = mb_substr($data, 0, 10, 'UTF-8') . "...";
             $data = trim($data);
         }
-    }
-    elseif ($fieldname == 'article_publish_time') 
-    {
+    } elseif ($fieldname == 'article_publish_time') {
         // 用当前采集时间戳作为发布时间
         $data = time();
-    }
-    // 把当前内容页URL替换上面的field
-    elseif ($fieldname == 'url') 
-    {
+    } // 把当前内容页URL替换上面的field
+    elseif ($fieldname == 'url') {
         $data = $page['url'];
     }
     return $data;
